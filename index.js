82d2c30b098271c37aaf62a94ce97ca87d90b132
Francky <gwadafrank@gmail.com>
2022-03-07 20:31:53 +0300

    commit tree 8fa9125c0d3772f1132ad0c5828b377a3f621a28

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((item) => {
+    const words  = item.split(" ");
+    const capitalized  = words.map(
+      (word) => word.charAt(0).toUpperCase()+ word.slice(1)
+    );
+    const res = capitalized.join(" ");
+    return res 
+  });
+};
